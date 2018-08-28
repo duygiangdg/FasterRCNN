85d43f8d066435b284e15a160163f5f41052b184
duygiang <duygiangdg@gmail.com>
2018-08-28 10:05:51 +0700

    fix loss

--- model_frcnn.py
+++ model_frcnn.py
@@ -166,8 +166,7 @@ def fastrcnn_losses(labels, label_logits, fg_boxes, fg_box_logits, fg_masks, mas
     box_loss = tf.losses.huber_loss(
         fg_boxes, fg_box_logits, reduction=tf.losses.Reduction.SUM)
     box_loss = tf.truediv(
-        box_loss, tf.to_float(tf.shape(labels)[0]), name='box_loss')
-    box_loss = 0.0
+        0.0, tf.to_float(tf.shape(labels)[0]), name='box_loss')
 
     mask_loss = tf.losses.huber_loss(
         fg_masks, mask_logits, reduction=tf.losses.Reduction.SUM)
