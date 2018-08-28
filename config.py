048e4b24d149dddb3452f516a0f0f81c0b37bd02
duygiang <duygiangdg@gmail.com>
2018-08-28 10:59:04 +0700

    change config

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ _C.DATA.NUM_CATEGORY = 80    # 80 categories.
 _C.DATA.CLASS_NAMES = []  # NUM_CLASS (NUM_CATEGORY+1) strings, to be populated later by data loader. The first is BG.
 
 # basemodel ----------------------
-_C.BACKBONE.WEIGHTS = '../ImageNet-ResNet50.npz'   # /path/to/weights.npz
+_C.BACKBONE.WEIGHTS = '../../ImageNet-ResNet50.npz'   # /path/to/weights.npz
 _C.BACKBONE.RESNET_NUM_BLOCK = [3, 4, 6, 3]     # for resnet50
 # RESNET_NUM_BLOCK = [3, 4, 23, 3]    # for resnet101
 _C.BACKBONE.FREEZE_AFFINE = False   # do not train affine parameters inside norm layers
