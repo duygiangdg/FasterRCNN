a22c0f4481eb50edd88739e410cabaa6f3fd8fd5
duygiang <duygiangdg@gmail.com>
2018-08-30 19:06:43 +0700

    fix viz

--- eval.py
+++ eval.py
@@ -83,7 +83,7 @@ def detect_one_image(img, model_func):
         # full_masks = [fill_full_mask(box, mask, orig_shape)
         #               for box, mask in zip(boxes, masks[0])]
         # masks = full_masks
-        print('mask', masks[0,1])
+        print('mask', masks[0][1])
     else:
         # fill with none
         masks = [None] * len(boxes)
