dd73f929a0bf29e651c96b4c4449479c5f7ac966
duygiang <duygiangdg@gmail.com>
2018-09-04 23:30:58 +0700

    fix bug

--- model_frcnn.py
+++ model_frcnn.py
@@ -120,7 +120,7 @@ def fastrcnn_outputs(feature, num_classes):
     box_regression = FullyConnected('box', feature, num_classes * 4,
         kernel_initializer=tf.random_normal_initializer(stddev=0.001))
     box_regression = tf.reshape(box_regression, (-1, num_classes, 4), name='output_box')
-    angle_regression = FullyConnected('angle', feature, 1,
+    angle_regression = FullyConnected('angle', feature, num_classes,
         kernel_initializer=tf.random_normal_initializer(stddev=0.01))
     angle_regression = tf.reshape(angle_regression, [-1], name='output_angle')
     return classification, box_regression, angle_regression
@@ -161,8 +161,8 @@ def fastrcnn_losses(labels, label_logits, fg_boxes, fg_box_logits, angles, angle
     box_loss = tf.losses.huber_loss(fg_boxes, fg_box_logits, reduction=tf.losses.Reduction.SUM)
     box_loss = tf.truediv(box_loss, tf.to_float(tf.shape(labels)[0]), name='box_loss')
 
-    angle_loss = tf.losses.mean_squared_error(angles, angle_logits)
-    angle_loss = tf.reduce_mean(angle_loss, name='mask_loss')
+    angle_loss = tf.losses.huber_loss(angles, angle_logits, reduction=tf.losses.Reduction.SUM)
+    angle_loss = tf.truediv(angle_loss, tf.to_float(tf.shape(labels)[0]), name='mask_loss')
 
     add_moving_summary(label_loss, box_loss, angle_loss, accuracy, fg_accuracy, false_negative)
     return label_loss, box_loss, angle_loss
